a297abe31f2d250f22c49f394e1834ea1fbb72e0
Valmir de Lima <valmirblima@hotmail.com>
2024-04-07 18:52:15 -0300

    First commit

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: '',
-    sendgridKey: '',
+    sendgridKey: 'SUA CONNECTION STRING',
     containerConnectionString: 'SUA CONNECTION STRING'
 }
