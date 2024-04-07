239bd80f9a5a2c82c2e4b32d9c87f6dd21661eb5
Valmir de Lima <valmirblima@hotmail.com>
2024-04-07 18:50:45 -0300

    First commit

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.SALT_KEY = '';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb://balta:e296cd9f@localhost:27017/admin',
-    sendgridKey: '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',
+    connectionString: '',
+    sendgridKey: '',
     containerConnectionString: 'SUA CONNECTION STRING'
 }
